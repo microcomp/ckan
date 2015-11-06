9e1711a9140c867ff7c6a4394cff08bf95ac7e2f
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-11-06 14:13:02 +0100

    datastore dump edited

--- ckanext/datastore/controller.py
+++ ckanext/datastore/controller.py
@@ -8,8 +8,10 @@ import ckan.plugins as p
 import ckan.lib.base as base
 import ckan.model as model
 
-
 from ckan.common import request, response
+
+import logging
+log = logging.getLogger(__name__)
 
 def _get_filename(resource_id, storage_path):
     directory = os.path.join(storage_path, 'resources', resource_id[0:3], resource_id[3:6])
@@ -18,7 +20,7 @@ def _get_filename(resource_id, storage_path):
 
 class DatastoreController(base.BaseController):
     def dump(self, resource_id):
-        limit = pylons.config.get('datastore.dump.limit', 10000)
+        limit = int(pylons.config.get('datastore.dump.limit', 10000))
         context = {
             'model': model,
             'session': model.Session,
@@ -27,11 +29,23 @@ class DatastoreController(base.BaseController):
 
         data_dict = {
             'resource_id': resource_id,
-            'limit': limit,
+            'limit': 1,
             'offset': request.GET.get('offset', 0)
         }
+        
+        small_resource = False
+        action = p.toolkit.get_action('datastore_search')
+        try:
+            result = action(context, data_dict)
+            if result.get('total', 0) <= limit:
+                small_resource = True
+        except p.toolkit.ObjectNotFound:
+            base.abort(404, p.toolkit._('DataStore resource not found'))
+        
         request_all = request.GET.get('all', False)
-        if request_all:
+        
+        #handle big resources if requested all data
+        if request_all and not small_resource:
             #try to provide file instead of generating CSV
             storage_path = pylons.config.get('ckan.storage_path')
             filepath = _get_filename(resource_id, storage_path)
@@ -45,9 +59,13 @@ class DatastoreController(base.BaseController):
                 fapp = FileApp(filepath, headers=headers)
                 return fapp(request.environ, self.start_response)
             else:
-                base.abort(404, _('Resource not found'))
+                base.abort(404, p.toolkit._('Resource consists of more than {0} records. Therefore this resource will be converted to CSV file in order to provided for users. Conversion will be executed within 24 hours from upload. Please try it again later.').format(limit))
 
-        action = p.toolkit.get_action('datastore_search')
+        data_dict = {
+            'resource_id': resource_id,
+            'limit': limit,
+            'offset': request.GET.get('offset', 0)
+        }
         try:
             result = action(context, data_dict)
         except p.toolkit.ObjectNotFound:
