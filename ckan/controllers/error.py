fb3467b6d3e6735d6cb4cb118bf4931ef34d0876
János Farkas <farkas48@uniba.sk>
2016-03-02 15:13:20 +0100

    no hack patch :octocat:

--- ckan/controllers/error.py
+++ ckan/controllers/error.py
@@ -4,9 +4,11 @@ from paste.urlparser import PkgResourcesParser
 from pylons import request, tmpl_context as c
 from pylons.controllers.util import forward
 from webhelpers.html.builder import literal
-
+import logging
+import urllib
 from ckan.lib.base import BaseController
 from ckan.lib.base import render
+import ckan.lib.base as base
 
 
 class ErrorController(BaseController):
@@ -22,7 +24,15 @@ class ErrorController(BaseController):
 
     def document(self):
         """Render the error document"""
-        original_request = request.environ.get('pylons.original_request')
+        original_request = request.environ.get('pylons.original_request')        
+        if (original_request.referer != None) and (('%22'  in original_request.referer) or ('%3C' in original_request.referer) or ('(' in original_request.referer)) and ('fanstatic' in original_request.referer):
+            base.abort(400)
+            #return base.redirect(controller="home", action="index")
+        if (original_request.path != None) and (('%22'  in original_request.path) or ('%3C' in original_request.path) or ('(' in original_request.path)) and ('fanstatic' in original_request.path):
+            base.abort(400)
+            #return base.redirect(controller="home", action="index")
+        
+        #original_request = urllib.urlencode(original_request)
         original_response = request.environ.get('pylons.original_response')
         # When a request (e.g. from a web-bot) is direct, not a redirect
         # from a page. #1176
